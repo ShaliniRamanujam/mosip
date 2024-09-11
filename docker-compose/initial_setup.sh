f2df9f44ac57855b9c3851c9ddb8acb6b850ed4e
shalini <shalini.ramanujam@thoughtworks.com>
2024-09-11 18:11:08 +0530

    Remove commands for mock setup

--- docker-compose/initial_setup.sh
+++ docker-compose/initial_setup.sh
@@ -1,10 +1,3 @@
-cd ./docker-compose-certify
-sed -i "/^  esignet:/,/^  [^ ]/s/   - active_profile_env=.*$/   - active_profile_env=default,mock-identity/" docker-compose.yml
-sed -i "/^  certify:/,/^  [^ ]/s/   - active_profile_env=.*$/   - active_profile_env=default,mock-identity/" docker-compose.yml
-sed -i "s|- esignet_wrapper_url_env|#&|" docker-compose.yml
-sed -i "s|#      - enable_certify_artifactory|      - enable_certify_artifactory|" docker-compose.yml
-sed -i "s|#      - ./loader_path/certify|      - ./loader_path/certify|" docker-compose.yml
-cd ..
 ./install.sh < 1
 cd ..
 curl -X POST http://localhost:8000/identity/did/generate -H "Content-Type: application/json" -d '{
